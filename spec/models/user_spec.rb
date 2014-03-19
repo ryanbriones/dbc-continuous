e395c1324b418a58e63f0ce873afd3daa13d299e
Ryan Briones <ryan.briones@brionesandco.com>
2014-03-19 08:18:39 -0500

    added Users with a name

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe User do
+  context "when displaying the user's entire name" do
+    it "is a combination of first and last name" do
+      user = User.new(first_name: "Ryan", last_name: "Briones")
+      expect(user.name).to eq("Ryan Briones")
+    end
+  end
+end
